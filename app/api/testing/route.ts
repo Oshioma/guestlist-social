eff1feae0561b2504ba62345a4d4921e790f57cc
Oshioma <oshi@guestlist.net>
2026-04-14 00:54:10 +0300

    Update route.ts

--- app/api/testing/route.ts
+++ app/api/testing/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-export async function GET(
-  req: NextRequest,
-  context: { params: { slug: string } }
-) {
-  return NextResponse.json({ ok: true, params: context.params });
+
+// This endpoint responds to GET requests at /api/testing
+export async function GET(req: NextRequest) {
+  return NextResponse.json({ ok: true });
 }
